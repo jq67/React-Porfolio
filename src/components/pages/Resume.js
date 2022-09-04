cd58548971f58ab4fa88b29113ec5ad003e09593
Julian Quan Fun <julianquanfun@gmail.com>
2022-09-04 14:18:21 -0400

    added something

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import jqResume from '../../assets/jquanfunResume.pdf'
+import coverLetter from '../../assets/coverLetter.pdf'
 import screenShot from '../../assets/resumeScreen.png'
 
 
@@ -7,6 +8,7 @@ export default function Resume() {
   return (
     <div className="text-center">
       <h1 className="anybody m-3">Resume</h1>
+      <a className="p-3" href={coverLetter} download="coverLetter"><h3>Cover Letter</h3></a>
       <a className="p-3" href={jqResume} download="jqResume"><h3>Download</h3></a>
       <div>
         <img src={screenShot} alt="resume screenshot"></img>
